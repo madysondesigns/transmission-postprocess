1b50d8cfc5a415e5b9870f2b0408d3b2f64365b5
Sarah Whinnem <sarah@madysondesigns.com>
2023-03-17 17:24:00 -0400

    fix for zsh, also fail less invisibly

--- transmission-postprocess.sh
+++ transmission-postprocess.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh -xu
+#!/usr/bin/env zsh
 
-# Transmission passes in vars for torrent info:
+# Input: Transmission passes in vars for torrent info:
 # TR_APP_VERSION - Transmission's short version string, e.g. 4.0.0
 # TR_TIME_LOCALTIME
 # TR_TORRENT_BYTES_DOWNLOADED - Number of bytes that were downloaded for this torrent
@@ -11,23 +11,29 @@
 # TR_TORRENT_NAME - Name of torrent (not filename)
 # TR_TORRENT_TRACKERS - A comma-delimited list of the torrent's trackers' announce URLs
 
-# Get custom env stuff so we have vars and homebrew – non-interactive shells don't get it automatically 🤷‍♀️
-source $HOME/.zshenv
-
-# Configuration
-# Get working directories from environment vars
-PROCESSING_PATH="$DROPBOXDIR/Downloads"
-PLEX_PATH="$NASDIR/Plex"
-# Set up other config we'll need later
+# Config: Helpers for logging & notification
 LOG_DATE="+%F %T"
-DOWNLOAD_PATH="$TR_TORRENT_DIR/$TR_TORRENT_NAME"
-LOG_FILE="$PROCESSING_PATH/Logs/$(date '+%Y%m%d@%H%M')-$TR_TORRENT_NAME.txt"
 NOTIFY_SHORTCUT="Notify All Devices"
 
-# Create log for date and file
-echo "[$(date "$LOG_DATE")] Starting postprocess for $TR_TORRENT_NAME..." > $LOG_FILE
+# Config: Get environment vars and store working directories
+[[ -d $DROPBOXDIR ]] && PROCESSING_PATH="$DROPBOXDIR/Downloads"
+[[ -d $NASDIR ]] && PLEX_PATH="$NASDIR/Plex"
+DOWNLOAD_PATH="$TR_TORRENT_DIR/$TR_TORRENT_NAME"
+LOG_FILE="$PROCESSING_PATH/Logs/$(date '+%Y%m%d@%H%M')-$TR_TORRENT_NAME.txt"
 
-# Check if file matches common TV naming patterns
+# Config: Make sure we set things up properly and log/notify if not
+if [[ ! $PROCESSING_PATH || ! $PLEX_PATH ]]; then
+  ERROR_LOG="$HOME/Desktop/transmission-error.txt"
+  echo "[$(date "$LOG_DATE")] Transmission script error: Unable to set config from env variables, something seems broken..." >> "$ERROR_LOG"
+  echo "[$(date "$LOG_DATE")] Transmission script error: \$ZDOTDIR: $ZDOTDIR; \$DROPBOXDIR: $DROPBOXDIR; \$NASDIR: $NASDIR" >> "$ERROR_LOG"
+  printf "Transmission script error, check error log" | shortcuts run "$NOTIFY_SHORTCUT"
+  exit 1
+fi
+
+# Logging: Start a logfile for date and torrent
+echo "[$(date "$LOG_DATE")] Starting postprocess for $TR_TORRENT_NAME..." > "$LOG_FILE"
+
+# Setup: Check if file matches common TV naming patterns
 probably_tv () {
   local name=$1
 
@@ -38,13 +44,12 @@ probably_tv () {
   local pattern5="Series|series" # Series or series
   local pattern6="Episode|episode" # Episode or episode
 
-  local patterns=($pattern1 $pattern2 $pattern3 $pattern4 $pattern5 $pattern6)
+  local patterns=("$pattern1" "$pattern2" "$pattern3" "$pattern4" "$pattern5" "$pattern6")
   local description=("matches 'S00' pattern" "includes 'season' text" "matches 'E00' pattern" "matches '0x00' pattern" "includes 'series' text" "includes 'episode' text")
 
-  for pattern in ${!patterns[@]}; do
-    if (echo $name | grep -Eq  ${patterns[$pattern]}); then
-      echo "${description[$pattern]}"
-      return 0
+  for pattern in $patterns; do
+    if (echo $name | grep -Eq $pattern); then
+      echo "$description[${patterns[(ie)$pattern]}]" && return
     fi
   done
 
@@ -52,21 +57,22 @@ probably_tv () {
 }
 TV_MATCH=$(probably_tv "$TR_TORRENT_NAME")
 
-# Handle labels if we have them
+# Setup: Handle labels if we have them and log results
 if [[ $TR_TORRENT_LABELS ]]; then
-  echo "[$(date "$LOG_DATE")] Using for $TR_TORRENT_NAME passed from Transmission" >> $LOG_FILE
+  echo "[$(date "$LOG_DATE")] Using for $TR_TORRENT_NAME passed from Transmission" >> "$LOG_FILE"
   LABEL=$TR_TORRENT_LABELS
 elif [[ $TV_MATCH ]]; then
-  echo "[$(date "$LOG_DATE")] $TR_TORRENT_NAME $TV_MATCH, adding TV label" >> $LOG_FILE
+  echo "[$(date "$LOG_DATE")] $TR_TORRENT_NAME $TV_MATCH, adding TV label" >> "$LOG_FILE"
   LABEL="tv"
 else
-  echo "[$(date "$LOG_DATE")] No labels for $TR_TORRENT_NAME, allowing FileBot to detect" >> $LOG_FILE
+  echo "[$(date "$LOG_DATE")] No labels for $TR_TORRENT_NAME, allowing FileBot to detect" >> "$LOG_FILE"
   LABEL="N/A"
 fi
 
-echo "[$(date "$LOG_DATE")] Processing $TR_TORRENT_NAME with label: $LABEL; source: $DOWNLOAD_PATH; destination: $PLEX_PATH; size: $TR_TORRENT_BYTES_DOWNLOADED" >> $LOG_FILE
+# Logging: Add details about the torrent we've generated so far
+echo "[$(date "$LOG_DATE")] Processing $TR_TORRENT_NAME with label: $LABEL; source: $DOWNLOAD_PATH; destination: $PLEX_PATH; size: $TR_TORRENT_BYTES_DOWNLOADED" >> "$LOG_FILE"
 
-# Call FileBot to do the thing
+# Filebot: Call AMC script to do the thing
 FILEBOT=$(/usr/local/bin/filebot -script fn:amc \
   --output "$PLEX_PATH" \
   --action duplicate -non-strict \
@@ -79,9 +85,10 @@ FILEBOT=$(/usr/local/bin/filebot -script fn:amc \
     excludeList="$PROCESSING_PATH/.excludes" \
   )
 
-echo "\n$FILEBOT\n" >> $LOG_FILE
+# Logging: Add Filebot output
+echo "\n$FILEBOT\n" >> "$LOG_FILE"
 
-# Handle whether FileBot did the thing or not
+# Output: Handle whether FileBot did the thing or not and log results
 [[ $FILEBOT ]] && MATCHED=$(echo -e "$FILEBOT" | awk -F" " '/^Processed/{print $2}')
 if [[ $MATCHED -ge 1 ]]; then
   MEDIA=$(echo -e "$FILEBOT" | awk -F"\' | \'" '/^Execute/{print $2}')
@@ -91,18 +98,19 @@ else
   cp -r "$DOWNLOAD_PATH" "$PLEX_PATH/Unsorted" && NOTIFICATION+=" (copied to $PLEX_PATH/Unsorted)"
 fi
 
-# Trigger Shortcuts to send a notification
-echo "[$(date "$LOG_DATE")] Sending Shortcuts notification: $NOTIFICATION" >> $LOG_FILE
+# Notify: Trigger Shortcuts to send a notification and log
+echo "[$(date "$LOG_DATE")] Sending Shortcuts notification: $NOTIFICATION" >> "$LOG_FILE"
 printf "$NOTIFICATION" | shortcuts run "$NOTIFY_SHORTCUT"
 
-# Move .torrent file so Transmission doesn't pick it up again
+# Cleanup: Move .torrent file so Transmission doesn't pick it up again and log
 for FILE in "$PROCESSING_PATH/Media"/*
 do
   FILE_HASH="$(transmission-show "$FILE" | awk '/Hash:/{print $2}')"
-  if [[ $FILE_HASH = $TR_TORRENT_HASH ]]; then
-    echo "[$(date "$LOG_DATE")] Moving $(basename "$FILE") out of watched folder" >> $LOG_FILE
+  if [[ $FILE_HASH = "$TR_TORRENT_HASH" ]]; then
+    echo "[$(date "$LOG_DATE")] Moving $(basename "$FILE") out of watched folder" >> "$LOG_FILE"
     mv "$FILE" "$PROCESSING_PATH/Complete"
   fi
 done
 
-echo "[$(date "$LOG_DATE")] 🤖🤖🤖🤖 Completed postprocess!" >> $LOG_FILE
+## Logging: done!
+echo "[$(date "$LOG_DATE")] 🤖🤖🤖🤖 Completed postprocess!" >> "$LOG_FILE"
