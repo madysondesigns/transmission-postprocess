ca65dd38e15ed1370156379cec56039f8d45ac8b
Sarah Whinnem <sarah@madysondesigns.com>
2023-03-23 18:06:49 -0400

    fix match parsing and polish logging

--- transmission-postprocess.sh
+++ transmission-postprocess.sh
@@ -11,26 +11,43 @@
 # TR_TORRENT_NAME - Name of torrent (not filename)
 # TR_TORRENT_TRACKERS - A comma-delimited list of the torrent's trackers' announce URLs
 
-# Config: Helpers for logging & notification
-LOG_DATE="+%F %T"
-NOTIFY_SHORTCUT="Notify All Devices"
-
-# Config: Get environment vars and store working directories
+# Config: Get environment vars and store working directories & helpers
 [[ -d $DROPBOXDIR ]] && PROCESSING_PATH="$DROPBOXDIR/Downloads"
 [[ -d $NASDIR ]] && PLEX_PATH="$NASDIR/Plex"
 DOWNLOAD_PATH="$TR_TORRENT_DIR/$TR_TORRENT_NAME"
 LOG_FILE="$PROCESSING_PATH/Logs/$(date '+%Y%m%d@%H%M')-$TR_TORRENT_NAME.txt"
+NOTIFY_SHORTCUT="Notify All Devices"
 
-# Config: Make sure we set things up properly and log/notify if not
+# Logging: Helper to make it less repetitve
+print_log () {
+  print "[$(date "+%F %T")] $1" >> "$LOG_FILE"
+}
+
+# Logging: Helper to make downloaded size easier to grok
+convert_size () {
+  local -F gigabytes=$(print -f "%.2f" "$(($1))e-9")
+  local -F megabytes=$(print -f "%.2f" "$(($1))e-6")
+  local LC_ALL=en_US.UTF-8
+
+  if [[ $gigabytes -ge 1 ]]; then
+    print -f "%'gGB" "$gigabytes"
+  elif [[ $megabytes -ge 1 ]]; then
+    print -f "%'.0fMB" "$megabytes"
+  else
+    print -f "%'gb" "$1"
+  fi
+}
+
+# Config: Make sure we set things up properly and log/notify/fail if not
 if [[ ! $PROCESSING_PATH || ! $PLEX_PATH ]]; then
-  ERROR_LOG="$HOME/Desktop/transmission-error.txt"
-  print "[$(date "$LOG_DATE")] Transmission script error: Unable to set config from env variables (\$ZDOTDIR: $ZDOTDIR; \$DROPBOXDIR: $DROPBOXDIR; \$NASDIR: $NASDIR)" >> "$ERROR_LOG"
-  printf "Transmission script error, check error log" | shortcuts run "$NOTIFY_SHORTCUT"
+  LOG_FILE="$HOME/Desktop/transmission-error.txt"
+  print_log "Transmission script error: Unable to set config from env variables (\$ZDOTDIR: $ZDOTDIR; \$DROPBOXDIR: $DROPBOXDIR; \$NASDIR: $NASDIR)"
+  print -n "Transmission script error, check error log" | shortcuts run "$NOTIFY_SHORTCUT"
   exit 1
 fi
 
 # Logging: Start a logfile for date and torrent
-print "[$(date "$LOG_DATE")] Starting postprocess for $TR_TORRENT_NAME..." > "$LOG_FILE"
+print_log "Starting postprocess for $TR_TORRENT_NAME..."
 
 # Setup: Check if file matches common TV naming patterns
 probably_tv () {
@@ -54,18 +71,18 @@ TV_MATCH=$(probably_tv "$TR_TORRENT_NAME")
 
 # Setup: Handle labels if we have them and log results
 if [[ $TR_TORRENT_LABELS ]]; then
-  print "[$(date "$LOG_DATE")] Using for $TR_TORRENT_NAME passed from Transmission" >> "$LOG_FILE"
+  print_log "Using for $TR_TORRENT_NAME passed from Transmission"
   LABEL=$TR_TORRENT_LABELS
 elif [[ $TV_MATCH ]]; then
-  print "[$(date "$LOG_DATE")] $TR_TORRENT_NAME $TV_MATCH, adding TV label" >> "$LOG_FILE"
+  print_log "$TR_TORRENT_NAME $TV_MATCH, adding TV label"
   LABEL="tv"
 else
-  print "[$(date "$LOG_DATE")] No labels for $TR_TORRENT_NAME, allowing FileBot to detect" >> "$LOG_FILE"
+  print_log "No labels for $TR_TORRENT_NAME, allowing FileBot to detect"
   LABEL="N/A"
 fi
 
 # Logging: Print the params and info we've generated so far
-print "[$(date "$LOG_DATE")] Processing $TR_TORRENT_NAME with label: $LABEL; source: $DOWNLOAD_PATH; destination: $PLEX_PATH; size: $TR_TORRENT_BYTES_DOWNLOADED" >> "$LOG_FILE"
+print_log "Processing $TR_TORRENT_NAME with label: $LABEL; source: $DOWNLOAD_PATH; destination: $PLEX_PATH; size: $(convert_size $TR_TORRENT_BYTES_DOWNLOADED)"
 
 # Filebot: Call AMC script to do the thing
 FILEBOT=$(/usr/local/bin/filebot -script fn:amc \
@@ -76,34 +93,34 @@ FILEBOT=$(/usr/local/bin/filebot -script fn:amc \
   --def unsorted=n music=n artwork=y \
     movieDB=TheMovieDB seriesDB=TheMovieDB::TV \
     ut_dir="$DOWNLOAD_PATH" ut_kind="multi" ut_title="$TR_TORRENT_NAME" ut_label="$LABEL" \
-    exec="printf {quote fn} &> /dev/null" \
+    exec="printf {quote primaryTitle} > /dev/null" \
     excludeList="$PROCESSING_PATH/.excludes" \
   )
 
 # Output: Handle whether FileBot did the thing or not and log results
-print "\n$FILEBOT\n" >> "$LOG_FILE"
-[[ $FILEBOT ]] && MATCHED=$(print -e "$FILEBOT" | awk -F" " '/^Processed/{print $2}')
-if [[ $MATCHED -ge 1 ]]; then
-  MEDIA=$(print -e "$FILEBOT" | awk -F"\' | \'" '/^Execute/{print $2}')
-  NOTIFICATION="$MEDIA ready to Plex! 🤖🎉"
+print_log "$FILEBOT"
+[[ $FILEBOT ]] && PROCESSED=$(awk '/^Processed/{print $2}' <<< "$FILEBOT")
+if [[ $PROCESSED -ge 1 ]]; then
+  TITLE=$(awk -F"\' | \'" '/^Execute/{print $2; exit}' <<< "$FILEBOT")
+  NOTIFICATION="$TITLE ready to Plex! 🤖🎉"
 else
   NOTIFICATION="$TR_TORRENT_NAME downloaded but not processed 🤖🤷‍♀️"
   cp -r "$DOWNLOAD_PATH" "$PLEX_PATH/Unsorted" && NOTIFICATION+=" (copied to $PLEX_PATH/Unsorted)"
 fi
 
 # Notify: Trigger Shortcuts to send a notification and log
-print "[$(date "$LOG_DATE")] Sending Shortcuts notification: $NOTIFICATION" >> "$LOG_FILE"
-printf "$NOTIFICATION" | shortcuts run "$NOTIFY_SHORTCUT"
+print_log "Sending Shortcuts notification: $NOTIFICATION"
+print -n "$NOTIFICATION" | shortcuts run "$NOTIFY_SHORTCUT"
 
 # Cleanup: Move .torrent file so Transmission doesn't pick it up again and log
 for FILE in "$PROCESSING_PATH/Media"/*
 do
   FILE_HASH="$(transmission-show "$FILE" | awk '/Hash:/{print $2}')"
   if [[ $FILE_HASH = "$TR_TORRENT_HASH" ]]; then
-    print "[$(date "$LOG_DATE")] Moving $(basename "$FILE") out of watched folder" >> "$LOG_FILE"
+    print_log "Moving $(basename "$FILE") out of watched folder"
     mv "$FILE" "$PROCESSING_PATH/Complete"
   fi
 done
 
 ## Logging: done!
-print "[$(date "$LOG_DATE")] 🤖🤖🤖🤖 Completed postprocess!" >> "$LOG_FILE"
+print_log "🤖🤖🤖🤖 Completed postprocess!"
