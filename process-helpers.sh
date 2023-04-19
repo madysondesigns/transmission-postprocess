82030a00a5a8a25d7eacae7e20e84e14ea322fa9
Sarah Whinnem <sarah@madysondesigns.com>
2023-04-18 21:51:55 -0400

    refactor functions into helpers file

---
+++ process-helpers.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env zsh
+
+# Helpers for Transmission/FileBot processing
+
+# Make logging less repetitve
+print_log () {
+  if [[ -z $LOG_FILE ]]; then
+    print "Script error: \$LOG_FILE not defined" && exit 1
+  else
+    print "[$(date "+%F %T")] $1" | tee -a "$LOG_FILE"
+  fi
+}
+
+# Make downloaded size easier to grok
+convert_size () {
+  local -F gigabytes=$(print -f "%.1f" "$(($1))e-9")
+  local -F megabytes=$(print -f "%.0f" "$(($1))e-6")
+  local LC_ALL=en_US.UTF-8
+
+  [[ $gigabytes -ge 1 ]] && print -f "%'gGB" "$gigabytes" && return
+  [[ $megabytes -ge 1 ]] && print -f "%'.0fMB" "$megabytes" && return
+  print -f "%'gb" "$1" && return
+}
+
+# Check if file matches common TV naming patterns
+probably_tv () {
+  local name=$1
+
+  declare -A newpats
+  newpats["matches 'S00' pattern"]="[S|s][0-9]{1,4}"
+  newpats["includes 'season' string"]="Season|season"
+  newpats["matches 'E00' pattern"]="[E|e]p?[0-9]{1,2}"
+  newpats["matches '0x00' pattern"]="[0-9]{1,2}x[0-9]{1,2}"
+  newpats["includes 'series' string"]="Series|series"
+  newpats["includes 'episode' string"]="Episode|episode"
+
+  for description pattern in ${(kv)newpats}; do
+    [[ "$name" =~ "$pattern" ]] && print "${(Q)description}" && return
+  done
+
+  return 1
+}
+
+# Handle labels if we have them and log results
+# TODO: figure out how to log this
+generate_label () {
+  # local tv_match=$(probably_tv "$1")
+  # local label="N/A"
+
+  # if [[ $TR_TORRENT_LABELS ]]; then
+  #   print_log "Using labels for $1 passed from Transmission"
+  #   label=$TR_TORRENT_LABELS
+  # elif [[ $tv_match ]]; then
+  #   print_log "$1 $tv_match, adding TV label"
+  #   label="tv"
+  # else
+  #   print_log "No labels for $1, allowing FileBot to detect"
+  # fi
+
+  [[ $TR_TORRENT_LABELS ]] && print -f "$TR_TORRENT_LABELS" && return
+  probably_tv "$1" > /dev/null && print -f "tv" && return
+  print -f "N/A" && return
+
+}
+
+# Find the "Processed # files" string and parse the number
+parse_processed () {
+  local processed=$(sed -En "s/^Processed ([0-9]+).*$/\1/p" <<< "$1")
+  local total=$(paste -sd+ - <<< $processed | bc)
+  print -f "$total" && return
+}
+
+# If any files were processed, find the exec output and parse the title
+parse_title () {
+  local title=$(sed -En "s/^Execute: : '(.+)'$/\1/p" <<< "$1" | head -1)
+  print -f "$title" && return
+}
--- test-filebot.sh
+++ test-filebot.sh
@@ -5,38 +5,49 @@
 # previously downloaded files to tweak FileBot options & formats.
 # Usage: `./test-filebot.sh "path/to/downloaded.torrent"`
 
+SCRIPT_PATH=$(cd "$(dirname "$BASH_SOURCE[0]")" > /dev/null && pwd)
+
+# TODO: make sure real script can get processing path and source path
+source "$SCRIPT_PATH/process-helpers.sh"
+
 # get torrent passed in as an arg and slice off name
 DOWNLOAD_PATH="$1"
 TR_TORRENT_NAME=$(basename "$DOWNLOAD_PATH")
 
 # boilerplate stuff that's auto-generated in the real script
-PLEX_PATH="$( cd "$( dirname "$BASH_SOURCE[0]" )" > /dev/null && pwd )/test"
-LABEL="tv"
+TEST_PATH="$SCRIPT_PATH/test"
+LOG_FILE="$TEST_PATH/$TR_TORRENT_NAME.txt"
 
 # finding things in scrollback sucks
-print "🟢🟢🟢🟢🟢 START TEST for $TR_TORRENT_NAME 🟢🟢🟢🟢🟢"
+print_log "🟢🟢🟢 Testing $TR_TORRENT_NAME 🟢🟢🟢"
+
+# set label
+# TODO: update real script to use helper
+LABEL=$(generate_label "$TR_TORRENT_NAME")
 
 # do the thing, using symlink action because it's faster
 FILEBOT=$(/usr/local/bin/filebot -script fn:amc \
-  --output "$PLEX_PATH" \
+  --output "$TEST_PATH" \
   --action symlink -non-strict \
-  --conflict auto \
+  --conflict override \
   --log-file amc.log \
-  --def unsorted=n music=n artwork=y \
+  --def unsorted=n music=n artwork=n \
     movieDB=TheMovieDB seriesDB=TheMovieDB::TV \
     ut_dir="$DOWNLOAD_PATH" ut_kind="multi" ut_title="$TR_TORRENT_NAME" ut_label="$LABEL" \
-    exec="printf {quote primaryTitle} > /dev/null" \
+    exec=": {quote primaryTitle}" \
   )
-print "$FILEBOT"
+print_log "$FILEBOT"
 
-# find the "Processed # files" string and parse the number
-[[ $FILEBOT ]] && PROCESSED=$(awk '/^Processed/{print $2}' <<< "$FILEBOT")
-print "Processed files: $PROCESSED"
+# check how many files were processed
+# TODO: update real script to use helper
+[[ $FILEBOT ]] && PROCESSED=$(parse_processed $FILEBOT)
+print_log "Processed $PROCESSED files"
 
-if [[ $PROCESSED -ge 1 ]]; then
-  # if any files were processed, find the "Execute: ..." string and parse the title
-  TITLE=$(awk -F"\' | \'" '/^Execute/{print $2; exit}' <<< "$FILEBOT")
-  print "$TITLE processed successfully! 🤖🎉"
+# log results
+if [[ $PROCESSED && $PROCESSED -ge 1 ]]; then
+  # TODO: update real script to use helper
+  TITLE=$(parse_title "$FILEBOT")
+  print_log "🤖🤖🤖 $TITLE processed successfully! 🎉 🤖🤖🤖"
 else
-  print "$TR_TORRENT_NAME not processed 🤖🤷‍♀️"
+  print_log "🤖🤖🤖 $TR_TORRENT_NAME not processed 🤷‍♀️ 🤖🤖🤖"
 fi
